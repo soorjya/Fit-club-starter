20c8e9eab4372ddfb0a9de134a61388dc235758c
Soorjyakanta Sethy <soorjyakant17@gmail.com>
2023-09-12 22:03:35 +0530

    hero updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,9 +8,10 @@ const Hero = () => {
         <div className="left-h">
             <Header/>
 
-            <div className="the-best-ad"></div>
+            <div className="the-best-ad">
             <div></div>
             <span>the best fitness club around</span>
+        </div>
         </div>
         <div className="right-h">Right Side</div>
     </div>   
