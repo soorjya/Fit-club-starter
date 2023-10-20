c16afbd5cf109ab0e6e164d6f5f94c03dfacbeec
Soorjyakanta Sethy <soorjyakant17@gmail.com>
2023-10-20 20:26:10 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
-import './Hero.css';
-import Header from '../Header/Header';
+import React from "react";
+import Header from "../Header/Header";
+import "./Hero.css";
 import hero_image from "../../assets/hero_image.png";
 import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
-
+import { motion } from "framer-motion";
+import NumberCounter from "number-counter";
+import {Link} from 'react-scroll'
 const Hero = () => {
+  const transition = { duration: 3, type: "spring" };
+  const mobile = window.innerWidth<=768? true:false;
   return (
     <div className="hero">
-        <div className="left-h">
-            <Header/>
+      <div className="blur hero-blur"></div>
 
-            <div className="the-best-ad">
-            <div></div>
-            <span>the best fitness club around</span>
-         </div>
-         {/*Hero heading */}
-         <div className="hero-text">
+      <div className="left-h">
+        <Header />
+        {/* the best ad */}
+        <div className="the-best-ad">
+          <motion.div
+            initial={{left: mobile? "165px": '238px' }}
+            whileInView={{ left: "8px" }}
+            transition={{ ...transition, type: "tween" }}
+          ></motion.div>
+          <span>THE BEST FITNESS CLUB IN THE TOWN</span>
+        </div>
+        {/* Hero text */}
+        <div className="hero-text">
           <div>
             <span className="stroke-text">Shape </span>
             <span>Your</span>
@@ -26,58 +36,89 @@ const Hero = () => {
             <span>Ideal body</span>
           </div>
           <div>
-          <span>
-          In here we will help you to shape and build your ideal body and live up your life to fullest
-          </span>
+            <span>
+              In here we will help you to shape and build your ideal body and
+              live up your life to fullest
+            </span>
           </div>
-         </div>
+        </div>
 
-         {/* figures */}
-         <div className="figures">
+        {/* experience figures */}
+        <div className="figures">
           <div>
-            <span>+140</span>
-            <span>expert coachs</span>
+            <span>
+              <NumberCounter end={140} start={100} delay={4} preFix="+" />
+            </span>
+            <span>expert coaches</span>
           </div>
           <div>
-            <span>+978</span>
-            <span>members joined</span>
-            </div>
+            <span>
+              <NumberCounter end={978} start={878} delay={4} preFix="+" />
+            </span>
+            <span>Members joined</span>
+          </div>
           <div>
-            <span>+50</span>
+            <span>
+              <NumberCounter end={50} delay={2} preFix="+" />
+            </span>
             <span>fitness programs</span>
           </div>
-         </div>
-
-         {/* hero buttons*/}
-         <div className="hero-buttons">
-          <buttons className="btn">Get Startred</buttons>
-          <buttons className="btn">Learn More</buttons>
-         </div>
         </div>
-        <div className="right-h">
-          <button className='btn'>Join now</button>
 
-
-          <div className="heart-rate">
-            <img src={Heart} alt="" />
-            <span>Heart Rate</span>
-            <span>116 bpm</span>
-          </div>
-          {/* Hero images */}
-          <img src={hero_image} alt="" className="hero-image" />
-          <img src={hero_image_back} alt="" className="hero-image-back" />
-          {/* Caloreis */}
-          <div className="calories">
-            <img src={Calories} alt="" />
-            <div>
-              <span>Calories Burned</span>
-              <span>220 kcal</span>
-            </div>
-          </div>
-
+        {/* hero buttons */}
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
         </div>
-    </div>   
-  )
-}
+      </div>
 
-export default Hero
+      {/* Right Side */}
+      <div className="right-h">
+        <button className="btn">
+          <Link to = 'join-us' smooth={true} spy={true}>
+          Join Now
+          </Link>
+          </button>
+
+        {/* heart rate */}
+        <motion.div
+          initial={{ right: "-1rem" }}
+          whileInView={{ right: "4rem" }}
+          transition={transition}
+          className="heart-rate"
+        >
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </motion.div>
+
+        {/* hero images */}
+        <img className="hero-image" src={hero_image} alt="" />
+        <motion.img
+          initial={{ right: "11rem" }}
+          whileInView={{ right: "20rem" }}
+          transition={transition}
+          className="hero-image-back"
+          src={hero_image_back}
+          alt=""
+        />
+
+        {/* calories */}
+        <motion.div
+          initial={{ right: "32rem" }}
+          whileInView={{ right: "28rem" }}
+          transition={transition}
+          className="calories"
+        >
+          <img src={Calories} alt="" />
+          <div>
+            <span>Calories burned</span>
+            <span>220 kcal</span>
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
