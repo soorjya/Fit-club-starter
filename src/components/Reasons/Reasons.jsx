07a6615d58dd62b8fbdc53108d84e216c722ec6c
Soorjyakanta Sethy <soorjyakant17@gmail.com>
2023-10-20 19:53:28 +0530

    Update Reasons.jsx

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import "./Reasons.css";
 import image1 from "../../assets/image1.png";
 import image2 from "../../assets/image2.png";
 import image3 from "../../assets/image3.png";
 import image4 from "../../assets/image4.png";
-import nb from "../../assets/nb.png";
-import adidas from "../../assets/adidas.png";
-import nike from "../../assets/nike.png";
+import nb from '../../assets/nb.png';
+import adidas from '../../assets/adidas.png'
+import nike from '../../assets/nike.png'
 import tick from "../../assets/tick.png";
-
+import "./Reasons.css";
 const Reasons = () => {
   return (
-    <div className="Reasons" id="reasons">
+    <div className="Reasons" id='reasons'>
       <div className="left-r">
         <img src={image1} alt="" />
         <img src={image2} alt="" />
@@ -21,40 +20,32 @@ const Reasons = () => {
       <div className="right-r">
         <span>some reasons</span>
         <div>
-          <span className="stroke-text">why</span>
-          <span> choose us?</span>
+          <span className="stroke-text">Why </span>
+          <span>choose us?</span>
         </div>
-
         <div className="details-r">
           <div>
             <img src={tick} alt="" />
-            <span>OVER 140+ EXPERT COACHS</span>
+            <span>over 140+ expert coachs</span>
           </div>
           <div>
             <img src={tick} alt="" />
-            <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
+            <span>train smarter and faster than before</span>
           </div>
           <div>
             <img src={tick} alt="" />
-            <span>1 FREE PROGRAM FOR NEW MEMBER</span>
+            <span>1 free program for new member</span>
           </div>
           <div>
             <img src={tick} alt="" />
-            <span>RELIABLE PARTNERS</span>
+            <span>reliable partners</span>
           </div>
         </div>
-        <span
-          style={{
-            color: "var(--gray)",
-            fontWeight: "normal",
-          }}
-        >
-          OUR PARTNERS
-        </span>
+        <span style={{color: "var(--gray)", fontWeight: "normal"}}>OUR PARTNERS</span>
         <div className="partners">
-          <img src={nb} alt="" />
-          <img src={adidas} alt="" />
-          <img src={nike} alt="" />
+              <img src={nb} alt="" />
+              <img src={adidas} alt="" />
+              <img src={nike} alt="" />
         </div>
       </div>
     </div>
